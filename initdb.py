ef9a4b7acb2bcadebcb224b15338f9fa6eea46a3
Samuel Kontiomaa <samuel.kontiomaa@gmail.com>
2019-05-08 23:33:16 +0400

    Separated database check and initialization to separate file

---
+++ initdb.py
@@ -0,0 +1,20 @@
+import os
+import sqlite3
+
+def initdb():
+    db_file = 'db/mediadb.db'
+    db_is_new = not os.path.exists(db_file)
+
+    if db_is_new:
+        connection = sqlite3.connect(db_file)
+        cursor = connection.cursor()
+
+        print ('Creating db from schema')
+        schema_filename = 'schema/schema.sql'
+        with open(schema_filename, 'rt') as sch:
+            cursor.executescript(sch.read())
+    else:
+        print ('Database exists')
+
+    cursor.close()
+    connection.close()
--- myMediaManager.py
+++ myMediaManager.py
@@ -1,28 +1,6 @@
-import os
-import sqlite3
+from initdb import initdb
 
-db_file = 'mediadb.db'
-
-db_is_new = not os.path.exists(db_file)
-
-connection = sqlite3.connect(db_file)
-cursor = connection.cursor()
-
-if db_is_new:
-    print ('Creating schema')
-    schema_filename = 'schema.sql'
-    with open(schema_filename, 'rt') as sch:
-        cursor.executescript(sch.read())
-else:
-    print ('Database exists')
-    #cursor.execute("insert into test values ('testing this out')")
-    #connection.commit()
-
-    #cursor.execute("select * from test")
-    #print(cursor.fetchone())
-
-cursor.close()
-connection.close()
+initdb()
 
 print('What do you want to do?')
 print('1. List')
