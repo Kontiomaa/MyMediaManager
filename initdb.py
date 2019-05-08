efd12b572ee988248c3d7c86f8d9b76aea3b888c
Samuel Kontiomaa <samuel.kontiomaa@gmail.com>
2019-05-08 23:39:54 +0400

    Fixed path of schema & db

--- initdb.py
+++ initdb.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 
 def initdb():
-    db_file = 'db/mediadb.db'
+    db_file = 'mediadb.db'
     db_is_new = not os.path.exists(db_file)
 
     if db_is_new:
@@ -10,7 +10,7 @@ def initdb():
         cursor = connection.cursor()
 
         print ('Creating db from schema')
-        schema_filename = 'schema/schema.sql'
+        schema_filename = 'schema.sql'
         with open(schema_filename, 'rt') as sch:
             cursor.executescript(sch.read())
     else:
